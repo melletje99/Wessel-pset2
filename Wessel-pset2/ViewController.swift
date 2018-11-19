ea46c0ae3ae734a136a48ed241bcb5ab0e5a8abf
melletje99 <wesselmel@gmail.com>
2018-11-19 11:31:36 +0100

    Initial Commit

---
+++ Wessel-pset2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wessel-pset2
+//
+//  Created by Wessel Mel on 19/11/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
